7c455b1ed3fedeebe776525fe3151f3520f0c112
KristianColville1 <kristiancolville1@gmail.com>
2022-06-09 11:48:11 +0100

    Add examples of this keyword usage and using conditonal statements

--- the-importance-of-this/js/script.js
+++ the-importance-of-this/js/script.js
@@ -1,3 +1,35 @@
+// Uses the 'this' key word to select the individual/specific paragraph to toggle
 $('p').click(function() {
-    $('p').slideToggle('slow');
+    $(this).slideToggle('slow');
+});
+
+$('button').mouseenter(function(){
+    $(this).removeClass('make-red').addClass('make-blue');
+});
+
+$('button').mouseleave(function(){
+    $(this).removeClass('make-blue').addClass('make-red');
+});
+
+// The document ready function
+$(document).ready(function(){
+    $('.box').on('click', function(){
+
+    	var classNames = $(this).attr("class").split(" ");
+    	var boxClass = classNames[0];
+    	var className = classNames[1];
+
+    	if ($(this).css("background-color") == "rgb(255, 0, 0)") {
+    		// If this object is already red, turn it black
+            
+    		$("." + className).css("background-color", "#000"); 
+    	} else {
+    		// Else turn all elements with a box class black
+    		// and then change the color of all elements
+    		// with the same class name as the clicked element
+    		// to red.
+    		$("." + boxClass).css("background-color", "#000");
+    		$("." + className).css("background-color", "red");
+    	}
+    });
 });
